d7c5454eb07801af7f849d60dd82d56fc6015bef
Fabio Ribeiro de Carvalho <fabio.ribeirodecarvalho@motorolasolutions.com>
2025-02-17 10:08:01 -0300

    finish the base project structure

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,18 +1,22 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "../user/entities/user.entity";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost", // Use the service name defined in docker-compose.yaml
-      port: 3306, // default MySQL port
-      username: "root", // replace with your MySQL username
-      password: "fabio", // replace with your MySQL password
-      database: "hack-freela", // replace with your MySQL database name
-      entities: [User],
-      synchronize: true, // Set to false in production
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: "mysql",
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_DATABASE"),
+        entities: [User],
+        synchronize: true, // Set to false in production
+      }),
+      inject: [ConfigService],
     }),
   ],
   exports: [TypeOrmModule],
